09e90452d2ff65c035c9480ba6f02e60b7c3cf8d
BalahaMaster <karpiesiuk.marcin@gmail.com>
2018-09-08 16:13:48 +0200

    created Match, Side and User classes

---
+++ Franking/Franking/Models/Match.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Franking.Models
+{
+    public class Match
+    {
+        public Side Home { get; set; }
+        public Side Away { get; set; }
+        public Side Winner
+        {
+            get { return Winner; }
+            set
+            {
+                if (Home.Score == Away.Score) Winner = null;
+                else if (Home.Score > Away.Score) Winner = Home;
+                else
+                    Winner = Away;
+                    
+            }
+        }
+
+        public Match(Side homeParam, Side awayParam)
+        {
+            this.Home = homeParam;
+            this.Away = awayParam;
+        }
+    }
+}
---
+++ Franking/Franking/Models/Side.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Franking.Models
+{
+    public class Side
+    {
+        // class created for clarity of Match class
+        public User Player { get; set; }
+        public Team Team { get; set; }
+        public int Score { get; set; }
+    }
+}
