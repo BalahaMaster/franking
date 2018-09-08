6e3e69772aeb143993d8fce81c50824a0d513c11
BalahaMaster <karpiesiuk.marcin@gmail.com>
2018-09-08 21:22:28 +0200

    installed entity framework and modified User class

--- Franking/DataAccessLayer/Models/User.cs
+++ Franking/DataAccessLayer/Models/User.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Franking.Models;
 
-namespace Franking.Models
+namespace DataAccessLayer.Models
 {
     public class User
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public List<Match> MatchList { get; set; }
     }
 }
